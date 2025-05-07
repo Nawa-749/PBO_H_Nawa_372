63de973109e45e7cd99689b22e3f7bfbdd2c1537
Nawa-749 <nawaistiqomah@gmail.com>
2025-05-07 22:10:51 +0700

    PBO

--- Demo/src/Admin.java
+++ Demo/src/Admin.java
@@ -3,11 +3,7 @@ public class Admin extends User {
     private String password;
 
     public Admin(String nama, String nim, String username, String password) {
-<<<<<<< HEAD
-        super(nama, nim);
-=======
         super(nama, nim); // Call the constructor of the parent class
->>>>>>> d34c4fda6f917d6c4f1604517c6c51b50ca85eb4
         this.username = username;
         this.password = password;
     }
@@ -36,10 +32,8 @@ public class Admin extends User {
             return false;
         }
     }
-<<<<<<< HEAD
-=======
+
     // Override displayInfo method
->>>>>>> d34c4fda6f917d6c4f1604517c6c51b50ca85eb4
     @Override
     public void displayInfo() {
         System.out.println("Login Admin Berhasil!");
